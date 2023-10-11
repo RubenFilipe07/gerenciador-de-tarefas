01f48738c11783f446aa99830bac196b114347a2
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-10-11 02:52:40 -0300

    feat: add project pages

--- frontend/gerenciador-tarefas/src/app/app.module.ts
+++ frontend/gerenciador-tarefas/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { TarefasComponent } from './pages/tarefas/tarefas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    LoginComponent,
+    CadastroComponent,
+    TarefasComponent
   ],
   imports: [
     BrowserModule,
