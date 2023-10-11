2b30ced38eb8b2666d9dcd8544bc55b6e55773b3
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-10-11 04:58:04 -0300

    feat: add ng-zorro-antd component library

--- frontend/gerenciador-tarefas/src/app/app.module.ts
+++ frontend/gerenciador-tarefas/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './pages/principal/principal.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { TarefasComponent } from './pages/tarefas/tarefas.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { pt_BR } from 'ng-zorro-antd/i18n'; 
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
@@ -19,8 +27,13 @@ import { TarefasComponent } from './pages/tarefas/tarefas.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: pt_BR }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
