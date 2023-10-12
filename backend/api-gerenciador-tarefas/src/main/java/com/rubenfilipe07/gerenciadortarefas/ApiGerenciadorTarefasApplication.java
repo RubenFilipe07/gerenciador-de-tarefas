4f11030988b2cf9162ed4d83ef53452f06a3ff50
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-10-11 22:09:37 -0300

    feat: add initial spring files

---
+++ backend/api-gerenciador-tarefas/src/main/java/com/rubenfilipe07/gerenciadortarefas/ApiGerenciadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.rubenfilipe07.gerenciadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciadorTarefasApplication.class, args);
+	}
+
+}
---
+++ backend/api-gerenciador-tarefas/src/test/java/com/rubenfilipe07/gerenciadortarefas/ApiGerenciadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubenfilipe07.gerenciadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
