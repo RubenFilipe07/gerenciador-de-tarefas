eb3561350704c2846af6c56be185057f7c5d2762
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-10-13 18:10:12 -0300

    feat(api): add usuario service

--- backend/api-gerenciador-tarefas/src/main/java/com/rubenfilipe07/gerenciadortarefas/controllers/UsuarioController.java
+++ backend/api-gerenciador-tarefas/src/main/java/com/rubenfilipe07/gerenciadortarefas/controllers/UsuarioController.java
@@ -1,74 +1,42 @@
 package com.rubenfilipe07.gerenciadortarefas.controllers;
 
+import com.rubenfilipe07.gerenciadortarefas.models.Usuario;
+import com.rubenfilipe07.gerenciadortarefas.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.rubenfilipe07.gerenciadortarefas.models.Usuario;
-import com.rubenfilipe07.gerenciadortarefas.repositories.UsuarioRepository;
-
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/usuarios")
 public class UsuarioController {
 
-	@Autowired
-	UsuarioRepository usuarioRepository;
+    @Autowired
+    private UsuarioService usuarioService;  // Inject the UsuarioService
 
-	@GetMapping("/usuarios/{id}")
-	public ResponseEntity<Object> getOneUsuario(@PathVariable long id) {
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneUsuario(@PathVariable long id) {
+        return usuarioService.getOneUsuario(id);
+    }
 
-		if (usuarioRepository.findById(id).isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado");
-		}
+    @GetMapping("/")
+    public ResponseEntity<List<Usuario>> getAllUsuarios() {
+        return usuarioService.getAllUsuarios();
+    }
 
-		else {
-			return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findById(id));
-		}
-	}
+    @PostMapping("/")
+    public ResponseEntity<Usuario> saveUsuario(@RequestBody Usuario usuario) {
+        return usuarioService.saveUsuario(usuario);
+    }
 
-	@GetMapping("/usuarios")
-	public ResponseEntity<List<Usuario>> getAllUsuarios() {
-		return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findAll());
-	}
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateUsuario(@PathVariable long id, @RequestBody Usuario usuario) {
+        return usuarioService.updateUsuario(id, usuario);
+    }
 
-	@PostMapping("/usuarios")
-	public ResponseEntity<Usuario> saveUsuario(@RequestBody Usuario usuario) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
-	}
-
-	@PutMapping("/usuarios/{id}")
-	public ResponseEntity<Object> updateUsuario(@PathVariable long id, @RequestBody Usuario usuario) {
-
-		if (usuarioRepository.findById(id).isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado");
-		} else {
-			return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
-
-		}
-
-	}
-
-	@DeleteMapping("/usuarios/{id}")
-	public ResponseEntity<Object> deleteOneUsuario(@PathVariable long id) {
-
-		if (usuarioRepository.findById(id).isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado");
-		} else {
-			usuarioRepository.deleteById(id);
-			return ResponseEntity.status(HttpStatus.OK).body("Usuario deletado com sucesso");
-
-		}
-
-	}
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteOneUsuario(@PathVariable long id) {
+        return usuarioService.deleteOneUsuario(id);
+    }
 }
