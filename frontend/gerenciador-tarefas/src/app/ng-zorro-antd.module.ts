685531fd3451b9fe828e27949ccc5cb6a523995d
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-10-14 21:33:45 -0300

    refactor(principal): improve content of main page

--- frontend/gerenciador-tarefas/src/app/ng-zorro-antd.module.ts
+++ frontend/gerenciador-tarefas/src/app/ng-zorro-antd.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb'
-
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 
 
 @NgModule({
-    exports: [
-        BrowserModule,
-        NzLayoutModule,
-        NzMenuModule,
-        NzBreadCrumbModule,
-        
-    ]
-    })
-
+  exports: [
+    BrowserModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
+    NzTableModule,
+    NzDividerModule,
+    NzButtonModule,
+    NzAlertModule,
+    NzFormModule,
+    NzInputModule,
+    NzSelectModule,
+    NzModalModule,
+    NzDatePickerModule,
+  ]
+})
 export class NgZorroAntdModule { }
