b34ace7add65a58795c9c67bcc8ee1a1ba8044ab
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-10-16 05:39:42 -0300

    fix(corsConfiguration): remove / from url

--- backend/api-gerenciador-tarefas/src/src/main/java/com/rubenfilipe07/gerenciadortarefas/configs/CorsConfiguration.java
+++ backend/api-gerenciador-tarefas/src/src/main/java/com/rubenfilipe07/gerenciadortarefas/configs/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://sigtarefas.web.app/")
+            .allowedOrigins("https://sigtarefas.web.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
