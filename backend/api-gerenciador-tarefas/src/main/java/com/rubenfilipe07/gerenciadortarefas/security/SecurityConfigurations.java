a89bd849e475b1eb4ea99bcfe7bb5f0363b1a68c
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-10-17 15:04:34 -0300

    feat: add CorsConfigurationSource

--- backend/api-gerenciador-tarefas/src/main/java/com/rubenfilipe07/gerenciadortarefas/security/SecurityConfigurations.java
+++ backend/api-gerenciador-tarefas/src/main/java/com/rubenfilipe07/gerenciadortarefas/security/SecurityConfigurations.java
@@ -1,5 +1,7 @@
 package com.rubenfilipe07.gerenciadortarefas.security;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
@@ -39,6 +44,17 @@ public class SecurityConfigurations {
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
+	
+	 @Bean
+	    CorsConfigurationSource corsConfigurationSource() {
+	        CorsConfiguration configuration = new CorsConfiguration();
+	        configuration.setAllowedHeaders(List.of("*")); 
+	        configuration.setAllowedOrigins(List.of("*"));
+	        configuration.setAllowedMethods(List.of("*"));
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", configuration);
+	        return source;
+	    }
 
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
